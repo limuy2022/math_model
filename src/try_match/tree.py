f400b45a578f6065d95d8c8965b46a23491c6a35
OMObuan <dubbyzhong@163.com>
2024-05-09 17:38:21 +0800

    change text

--- src/try_match/tree.py
+++ src/try_match/tree.py
@@ -71,7 +71,7 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(
         data_without_first_column, y_data, test_size=0.2, random_state=42
     )
-    # 创建线性回归模型
+    # 创建模型
     model = DecisionTreeRegressor()
     res = model.fit(X_train, y_train)
 
