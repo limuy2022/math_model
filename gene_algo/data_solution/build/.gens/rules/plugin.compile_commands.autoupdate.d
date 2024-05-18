c901c116d314f029d075bfb11c22c5de01dd4360
limuy <limuyang202011@163.com>
2024-05-18 13:37:46 +0800

    update

--- gene_algo/data_solution/build/.gens/rules/plugin.compile_commands.autoupdate.d
+++
@@ -1,9 +0,0 @@
-{
-    files = {
-        "/home/limuy/桌面/weather/gene_algo/data_solution/xmake.lua",
-        "/home/limuy/桌面/weather/gene_algo/data_solution/.xmake/linux/x86_64/xmake.conf"
-    },
-    values = {
-        "src/train_main.cpp"
-    }
-}
